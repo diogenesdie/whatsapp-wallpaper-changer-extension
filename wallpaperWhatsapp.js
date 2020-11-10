d4fb61f5349a405ad72006bd401f0d46d4946ca4
diogenesdie <53631214+diogenesdie@users.noreply.github.com>
2020-11-10 16:02:42 -0300

    Feature: Upload images from PC

--- wallpaperWhatsapp.js
+++ wallpaperWhatsapp.js
@@ -1,4 +1,5 @@
 let blocks;
+let uploadImageFile;
 let link = null;
 let inputLink;
 
@@ -10,32 +11,112 @@ chrome.storage.local.get('linkWallpaper', function(result){
     }
 })
 
-function sleep(ms) {
+const setCSS = () => {
+    const css = `.change-wallpaper{
+                    width: 2.2vh;
+                    cursor: pointer;
+                    padding: 40%;
+
+                }
+
+                .list-options-active{
+                    display: block !important;
+                }
+
+                .list-options{
+                    position: absolute;
+                    border-radius: 0.2vw;
+                    color: #009588;
+                    background-color: #262D31;
+                    margin-top: 0%;
+                    margin-left: -27%;
+                    width: 10vw;
+                    display: none;
+                    list-style: none;
+                    
+                }
+
+                .list-options ul{
+                    padding: 0.5vw;
+                }
+
+                .list-options input{
+                    position: absolute;
+                    width: 10vh;
+                    opacity: 0;
+                    font-size: 1;
+                    border: none;
+                }
+
+                .list-options input #file-upload-button{
+                    cursor: pointer;
+                }
+
+                .list-options ul:hover{
+                    font-weight: bold;
+                }
+
+    `
+
+    const style = document.createElement('style');
+    
+    style.appendChild(document.createTextNode(css));
+
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function changeWallpaper(){
-    inputLink = prompt("Insert the link of the image you want to use: \n(leave blank for no changes or cancel to set default wallpaper)");
-    if(inputLink == ""){
-        return;
-    }
+const changeWallpaper = () => {
+    uploadImageFile = document.querySelector("#image-wallpaper-upload");
 
-    if(inputLink){
-        if(!inputLink.toLocaleLowerCase().includes("http")){
-            alert("You must insert a link!")
+    if(uploadImageFile.files.length){
+        
+        const image = uploadImageFile.files[0];
+        uploadImageFile.value = "";
+        const reader = new FileReader();
+
+        reader.onload = function(event) {
+            link = event.target.result;
+            chrome.storage.local.set({'linkWallpaper': link}, function() {
+                console.log('Link is set to ' + link);
+            });
+            
+            setWallpaper();
+        };
+        
+        reader.readAsDataURL(image);
+
+    }
+    else{
+        inputLink = prompt("Insert the link of the image you want to use: \n(leave blank for no changes or cancel to set default wallpaper)");
+
+        if(inputLink == ""){
             return;
         }
+
+        if(inputLink){
+            if(!inputLink.toLocaleLowerCase().includes("http")){
+                alert("You must insert a link!")
+                return;
+            }
+        }
+
+        link = inputLink;
+
+        chrome.storage.local.set({'linkWallpaper': link}, function() {
+            console.log('Link is set to ' + link);
+        });
+
+        setWallpaper();
+
     }
-
-    link = inputLink;
-    chrome.storage.local.set({'linkWallpaper': link}, function() {
-        console.log('Link is set to ' + link);
-    });
-
-    setWallpaper();
+   
 }
 
-function setWallpaper(){
+const setWallpaper = () => {
     const chat = document.querySelector('#main');
     const defautlWallpaper = document.querySelector('._27lSL');
 
@@ -59,25 +140,62 @@ function setWallpaper(){
     
 }
 
-function createButton(){
+const createButton = () => {
     const header = document.getElementsByTagName('header');
+    const button = document.createElement('img');
+    const options = document.createElement('div');
+    const list = document.createElement('li');
+    const sendImage = document.createElement('ul');
+    const sendLink = document.createElement('ul');
+    const resetWallpaper = document.createElement('ul');
+    const uploadImage = document.createElement('input');
 
-    const button = document.createElement('span');
-    const textForButton = document.createTextNode("Change Wallpaper");
+    button.setAttribute("src", "https://gemolstpc.ga/reload.svg");
+    button.setAttribute("class", "change-wallpaper");
 
-    button.setAttribute("style", "cursor: pointer; color: #080808; background-color: #EEE; border: 0.1vw solid #EEE; border-radius: 1vw; padding: 1%");
-    button.appendChild(textForButton)
-    button.setAttribute('id', 'change-wallpaper');
+    uploadImage.setAttribute("type", "file");
+    uploadImage.setAttribute("id", "image-wallpaper-upload");
+
+    sendImage.appendChild(uploadImage);
+    sendImage.appendChild(document.createTextNode("Upload Image"));
+    sendLink.appendChild(document.createTextNode("Use Link"));
+    resetWallpaper.appendChild(document.createTextNode("Reset Wallpaper"));
+
+    list.appendChild(sendImage);
+    list.appendChild(sendLink);
+    list.appendChild(resetWallpaper);
+    list.setAttribute("class", "list-options")
+
     
-    header[0].appendChild(button);
+
+    options.setAttribute("class", "drop-options");
+    options.appendChild(button);
+    options.appendChild(list);
     
-    button.addEventListener('click', changeWallpaper);
+    header[0].appendChild(options);
+    
+    options.addEventListener('click', ()=>{
+        list.classList.toggle("list-options-active");
+    });
+    sendLink.addEventListener('click', changeWallpaper);
+    sendImage.addEventListener('input', changeWallpaper);
+    resetWallpaper.addEventListener('click', ()=>{
+
+        chrome.storage.local.set({'linkWallpaper': DEFAULT_WALLPAPER}, function() {
+            console.log('Link is set to ' + DEFAULT_WALLPAPER);
+            setWallpaper();
+        });
+
+        
+    
+    });
 }
 
-async function start(){
+const start = async () => {
 
     blocks = document.querySelector('#pane-side');
     if(blocks){
+        setCSS();
         createButton();
         blocks.addEventListener('click', setWallpaper);
     }
